dadb6b758df205d1c93374b3c3e1879fcf7d20cb
Adam Brzakala <adam.brz@onet.eu>
2023-12-09 14:55:05 +0100

    Add more settings

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import bpy
 
-from .visualizer_generator import VisualizerGenerator
+from .visualizer_generator import VisualizerGenerator, VisualizerSettings
 from .audio_processor import ProcessingSettings
 
 bl_info = {
@@ -28,13 +28,37 @@ class AudioVisProperties(bpy.types.PropertyGroup):
         name="Max Frequency [Hz]", default=16000, min=1, max=20000
     )
     sampling_duration_ms: bpy.props.FloatProperty(
-        name="Sampling duration [ms]", default=100, min=1e-6, max=1e6
+        name="Sampling duration [ms]",
+        description="Duration of sampling time for single step (the less duration, the faster changes can be visualized).",
+        default=100,
+        min=1e-6,
+        max=1e6,
     )
     use_log_y: bpy.props.BoolProperty(
-        name="Logarithmic Scale", default=True
+        name="Logarithmic Scale",
+        description="Use logarithmic scale for frequency band height.",
+        default=True,
     )
     scaling_factor: bpy.props.FloatProperty(
-        name="Scaling Factor", default=15
+        name="Scaling Factor",
+        description="Multiplier for frequency band height.",
+        default=15,
+    )
+    freq_band_spacing: bpy.props.FloatProperty(
+        name="Frequency Bands Spacing",
+        default=1,
+        description="Amount of space between frequency bands.",
+    )
+    two_sided: bpy.props.BoolProperty(
+        name="Two Sided Visualizer",
+        description="Scale frequency band in both directions.",
+        default=False,
+    )
+    freq_band_scale_xy: bpy.props.FloatVectorProperty(
+        name="Frequency Band Scale",
+        description="X and Y scale of frequency band.",
+        default=(1.0, 1.0),
+        size=2,
     )
 
 
@@ -50,11 +74,14 @@ class AudioVisLayout:
         col.prop(props, "max_frequency")
         col.prop(props, "frequency_band_count")
         col.prop(props, "sampling_duration_ms")
+        col.prop(props, "use_log_y")
 
         col.label(text="Output Options")
         col.prop(props, "collection_name")
+        col.prop(props, "freq_band_spacing")
+        col.prop(props, "freq_band_scale_xy")
         col.prop(props, "scaling_factor")
-        col.prop(props, "use_log_y")
+        col.prop(props, "two_sided")
 
 
 class AudioVisPropertiesPanelBase(bpy.types.Panel):
@@ -95,7 +122,18 @@ class OBJECT_OT_AudioVis(AudioVisLayout, bpy.types.Operator):
                 props.use_log_y,
             )
         )
-        self.vis_generator.generate_visualizer(context, props.collection_name, props.scaling_factor)
+        self.vis_generator.generate_visualizer(
+            context,
+            props.collection_name,
+            VisualizerSettings(
+                props.freq_band_spacing,
+                props.freq_band_scale_xy[0],
+                props.freq_band_scale_xy[1],
+                props.scaling_factor,
+                props.two_sided,
+            ),
+        )
+
         return {"FINISHED"}
 
 
--- visualizer_generator.py
+++ visualizer_generator.py
@@ -1,7 +1,19 @@
-import bpy
-
 from .audio_data import AudioData
 from .audio_processor import AudioProcessor, ProcessingSettings
+
+import bpy
+
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class VisualizerSettings:
+    bands_spacing: float = 1
+    band_scale_x: float = 1
+    band_scale_y: float = 1
+    scaling_factor: float = 15
+    two_sided: bool = False
 
 
 class VisualizerGenerator:
@@ -11,15 +23,18 @@ class VisualizerGenerator:
         self.audio_processor_settings = ProcessingSettings()
         self.context = bpy.context
 
-    def generate_visualizer(self, context, collection_name: str, scaling_factor: int = 15):
+    def generate_visualizer(
+        self,
+        context,
+        collection_name: str,
+        settings: VisualizerSettings = VisualizerSettings(),
+    ):
         self.context = context
         visualization_values = self.audio_processor.process(
             self.audio_data, self.audio_processor_settings
         )
         self._clear_previous_visualization(collection_name)
-        self._create_animated_cubes(
-            collection_name, visualization_values, scaling_factor
-        )
+        self._create_animated_cubes(collection_name, visualization_values, settings)
 
     def load_audio_data(self, audio_path: str):
         self.audio_data.load(audio_path)
@@ -38,59 +53,70 @@ class VisualizerGenerator:
                 bpy.data.meshes.remove(mesh)
             bpy.data.collections.remove(collection)
 
-    def _create_visualizer_collection(self, collection_name: str):
-        visualizer_collection = bpy.data.collections.new(collection_name)
-        self.context.scene.collection.children.link(visualizer_collection)
-        return visualizer_collection
-
-    def _create_animated_cubes(self, visualization_values, scaling_factor):
+    def _create_animated_cubes(
+        self,
+        collection_name: str,
+        visualization_values: Any,
+        settings: VisualizerSettings,
+    ):
         keyframe_offset = (
-            self.audio_processor_settings.step_duration_s * self.context.scene.render.fps
+            self.audio_processor_settings.step_duration_s
+            * self.context.scene.render.fps
         )
-        scaled_visualization_data = visualization_values * scaling_factor
+        scaled_visualization_data = visualization_values * settings.scaling_factor
 
-        visualizer_collection = self._create_visualizer_collection()
+        visualizer_collection = self._create_visualizer_collection(collection_name)
         material = bpy.data.materials.new(name="Material")
         starting_cursor_location = self.context.scene.cursor.location.copy()
 
         for i in range(self.audio_processor_settings.frequency_bands):
-            freq_band_visualizer = self._create_freq_band_visualizer(material, i)
+            freq_band_visualizer = self._create_freq_band_visualizer(material, i, settings)
+            visualizer_collection.objects.link(freq_band_visualizer)
 
-            self.context.scene.cursor.location = self.context.active_object.location
-            self.context.scene.cursor.location.z -= 0.5
-            bpy.ops.object.origin_set(type="ORIGIN_CURSOR")
+            if not settings.two_sided:
+                bpy.context.scene.cursor.location = freq_band_visualizer.location
+                bpy.context.scene.cursor.location.z -= 0.5
+                bpy.context.view_layer.objects.active = freq_band_visualizer
+                bpy.ops.object.origin_set(type="ORIGIN_CURSOR")
 
             self._create_animation(
-                scaled_visualization_data, keyframe_offset, i, freq_band_visualizer
+                keyframe_offset, scaled_visualization_data, i, freq_band_visualizer
             )
-
-            visualizer_collection.objects.link(freq_band_visualizer)
 
         self.context.scene.cursor.location = starting_cursor_location
 
     def _create_animation(
-        self, visualization_values, keyframe_offset, i, freq_band_visualizer
+        self,
+        keyframe_offset: float,
+        scaled_visualization_data: Any,
+        freq_band_number: int,
+        freq_band_visualizer: Any,
     ):
-        for j in range(visualization_values.shape[0]):
+        for j in range(scaled_visualization_data.shape[0]):
             freq_band_visualizer.scale = (
                 1,
                 1,
-                visualization_values[j, i],
+                scaled_visualization_data[j, freq_band_number],
             )
             freq_band_visualizer.keyframe_insert(
                 data_path="scale", index=2, frame=(j * keyframe_offset + 1)
             )
 
-    def _create_freq_band_visualizer(self, material, i):
+    def _create_visualizer_collection(self, collection_name: str):
+        visualizer_collection = bpy.data.collections.new(collection_name)
+        self.context.scene.collection.children.link(visualizer_collection)
+        return visualizer_collection
+
+    def _create_freq_band_visualizer(self, material: Any, freq_band_number: int, settings: VisualizerSettings):
         bpy.ops.mesh.primitive_cube_add(
             size=1,
             enter_editmode=False,
             align="WORLD",
-            location=(i * 2, 0, 0),
-            scale=(1, 1, 1),
+            location=(freq_band_number * (settings.band_scale_x + settings.bands_spacing), 0, 0),
+            scale=(settings.band_scale_x, settings.band_scale_y, 1),
         )
         freq_band_visualizer = self.context.active_object
-        freq_band_visualizer.name = f"f{i}"
+        freq_band_visualizer.name = f"f{freq_band_number}"
         freq_band_visualizer.data.materials.append(material)
 
         if len(freq_band_visualizer.users_collection) > 0:
