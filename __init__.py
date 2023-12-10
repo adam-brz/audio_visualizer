cfb0eb7cfa8af657fdafa44dfa4a681ab83c8f69
Adam Brzakala <adam.brz@onet.eu>
2023-12-10 20:38:58 +0100

    Update descriptions

--- __init__.py
+++ __init__.py
@@ -15,49 +15,67 @@ bl_info = {
 
 
 class AudioVisProperties(bpy.types.PropertyGroup):
-    audio_filepath: bpy.props.StringProperty(name="Audio Filepath", subtype="FILE_PATH")
+    audio_filepath: bpy.props.StringProperty(
+        name="Audio Filepath",
+        description="Path to audio file to process",
+        subtype="FILE_PATH",
+    )
     collection_name: bpy.props.StringProperty(
-        name="Output Collection Name", default="Visualizer"
+        name="Output Collection Name",
+        description="Name of collection for generated objects",
+        default="Visualizer",
     )
     frequency_band_count: bpy.props.IntProperty(
-        name="Frequency Bands", default=20, min=1, max=4000
+        name="Frequency Bands",
+        description="Number of frequency bands to generate visualization for",
+        default=20,
+        min=1,
+        max=4000,
     )
     min_frequency: bpy.props.IntProperty(
-        name="Min Frequency [Hz]", default=60, min=1, max=20000
+        name="Min Frequency [Hz]",
+        description="Minimum frequency of signal to process",
+        default=60,
+        min=1,
+        max=20000,
     )
     max_frequency: bpy.props.IntProperty(
-        name="Max Frequency [Hz]", default=16000, min=1, max=20000
+        name="Max Frequency [Hz]",
+        description="Maximum frequency of signal to process",
+        default=16000,
+        min=1,
+        max=20000,
     )
     sampling_duration_ms: bpy.props.FloatProperty(
         name="Sampling duration [ms]",
-        description="Duration of sampling time for single step (the less duration, the faster changes can be visualized).",
+        description="Duration of sampling time for single step (the less duration, the faster changes can be visualized)",
         default=100,
         min=1e-6,
         max=1e6,
     )
     use_log_y: bpy.props.BoolProperty(
         name="Logarithmic Scale",
-        description="Use logarithmic scale for frequency band height.",
+        description="Use logarithmic scale for frequency band height",
         default=True,
     )
     scaling_factor: bpy.props.FloatProperty(
         name="Scaling Factor",
-        description="Multiplier for frequency band height.",
+        description="Multiplier for frequency band height",
         default=15,
     )
     freq_band_spacing: bpy.props.FloatProperty(
-        name="Frequency Bands Spacing",
+        name="Spacing Between",
         default=1,
-        description="Amount of space between frequency bands.",
+        description="Amount of space between frequency bands",
     )
     two_sided: bpy.props.BoolProperty(
         name="Two Sided Visualizer",
-        description="Scale frequency band in both directions.",
+        description="Scale frequency band in both directions",
         default=False,
     )
     freq_band_scale_xy: bpy.props.FloatVectorProperty(
         name="Frequency Band Scale",
-        description="X and Y scale of frequency band.",
+        description="X and Y scale of frequency band",
         default=(1.0, 1.0),
         size=2,
     )
@@ -67,21 +85,21 @@ class AudioVisLayout:
     def draw(self, context):
         props = context.window_manager.audio_vis
         layout = self.layout
-        col = layout.column()
 
-        col.label(text="Processing Options")
+        col = layout.column(heading="Processing Options")
         col.prop(props, "audio_filepath")
         col.prop(props, "min_frequency")
         col.prop(props, "max_frequency")
         col.prop(props, "frequency_band_count")
         col.prop(props, "sampling_duration_ms")
         col.prop(props, "use_log_y")
+        col.separator()
 
-        col.label(text="Output Options")
+        col = layout.column(heading="Output Options")
         col.prop(props, "collection_name")
-        col.prop(props, "freq_band_spacing")
         col.prop(props, "freq_band_scale_xy")
         col.prop(props, "scaling_factor")
+        col.prop(props, "freq_band_spacing")
         col.prop(props, "two_sided")
 
 
